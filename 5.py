19a611d670df3bb2a122fad1ac5d36ffd27ffc0c
sebastian0820 <sebastian.plata.dev@gmail.com>
2024-09-20 09:43:44 -0400

    updateed

--- 5.py
+++ 5.py
@@ -32,12 +32,49 @@ def token_ids_to_text(token_ids, tokenizer):
     flat = token_ids.squeeze(0) # remove batch dimension
     return tokenizer.decode(flat.tolist())
 
-start_context = "Every effort moves you"
 tokenizer = tiktoken.get_encoding("gpt2")
-token_ids = generate_text_simple(
-    model=model,
-    idx=text_to_token_ids(start_context, tokenizer),
-    max_new_tokens=10,
-    context_size=GPT_CONFIG_124M["context_length"]
-)
-print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
+# start_context = "Every effort moves you"
+# token_ids = generate_text_simple(
+#     model=model,
+#     idx=text_to_token_ids(start_context, tokenizer),
+#     max_new_tokens=10,
+#     context_size=GPT_CONFIG_124M["context_length"]
+# )
+# print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
+
+inputs = torch.tensor([[16833, 3626, 6100], # ["every effort moves",
+                       [40, 1107, 588]]) # "I really like"]
+targets = torch.tensor([[3626, 6100, 345 ], # [" effort moves you",
+                        [588, 428, 11311]]) # " really like chocolate"]
+with torch.no_grad():logits = model(inputs)
+probas = torch.softmax(logits, dim=-1) # Probability of each token in vocabulary
+# print(probas.shape)                                                                   #print tokens shape
+token_ids = torch.argmax(probas, dim=-1, keepdim=True)
+# print("Token IDs:\n", token_ids)                                                    #print tokens shape
+# print()
+# print(f"Targets batch 1: {token_ids_to_text(targets[0], tokenizer)}")               #print result for first token
+# print(f"Outputs batch 1: {token_ids_to_text(token_ids[0].flatten(), tokenizer)}")
+# print()
+# print(f"Targets batch 2: {token_ids_to_text(targets[1], tokenizer)}")               #print result for second token
+# print(f"Outputs batch 2: {token_ids_to_text(token_ids[1].flatten(), tokenizer)}")
+# print()
+text_idx = 0                                                                        #probability array for each token
+target_probas_1 = probas[text_idx, [0, 1, 2], targets[text_idx]]
+# print("Text 1:", target_probas_1)
+# print()
+text_idx = 1
+target_probas_2 = probas[text_idx, [0, 1, 2], targets[text_idx]]
+# print("Text 2:", target_probas_2)
+# print()
+log_probas = torch.log(torch.cat((target_probas_1, target_probas_2)))
+print(log_probas)
+print()
+avg_log_probas = torch.mean(log_probas)
+print(avg_log_probas)
+print()
+neg_avg_log_probas = avg_log_probas * -1
+print(neg_avg_log_probas)
+print()
+# loss = neg_avg_log_probas.backward()
+# print(loss)
+# print()
--- evaluation backbone.py
+++ evaluation backbone.py
@@ -17,15 +17,21 @@ torch.manual_seed(123)          #set randomization seed to make predictions repr
 model = GPTModel(GPT_CONFIG_124M)
 
 start_context = "Every effort moves you"
-encoded = tokenizer.encode(start_context)
-encoded_tensor = torch.tensor(encoded).unsqueeze(0)
-model.eval() #A
-out = generate_text_simple(
+def text_to_token_ids(text, tokenizer):
+    encoded = tokenizer.encode(text, allowed_special={'<|endoftext|>'})
+    encoded_tensor = torch.tensor(encoded).unsqueeze(0) # add batch dimension
+    return encoded_tensor
+
+def token_ids_to_text(token_ids, tokenizer):
+    flat = token_ids.squeeze(0) # remove batch dimension
+    return tokenizer.decode(flat.tolist())
+
+start_context = "Every effort moves you"
+tokenizer = tiktoken.get_encoding("gpt2")
+token_ids = generate_text_simple(
     model=model,
-    idx=encoded_tensor,
-    max_new_tokens=6,
+    idx=text_to_token_ids(start_context, tokenizer),
+    max_new_tokens = 6,
     context_size=GPT_CONFIG_124M["context_length"]
 )
-decoded_text = tokenizer.decode(out.squeeze(0).tolist())
-print(decoded_text)
-
+print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
