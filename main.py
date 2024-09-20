8a93db85fbd84280c68e84112c5c5eb1e703eab9
sebastian0820 <sebastian.plata.dev@gmail.com>
2024-09-19 21:05:43 -0400

    updated

---
+++ main.py
@@ -0,0 +1,33 @@
+import tiktoken
+import torch
+import torch.nn as nn
+from Gpt/GPTModel import GPTModel
+from GenTxt/generate_text_simple import generate_text_simple
+from GenTxt/print_gradients import print_gradients
+
+GPT_CONFIG_124M = {
+    "vocab_size": 50257, # Vocabulary size
+    "context_length": 1024, # Context length
+    "emb_dim": 768, # Embedding dimension
+    "n_heads": 12, # Number of attention heads
+    "n_layers": 12, # Number of layers
+    "drop_rate": 0.1, # Dropout rate
+    "qkv_bias": False # Query-Key-Value bias
+}
+tokenizer = tiktoken.get_encoding("gpt2")
+torch.manual_seed(123)
+model = GPTModel(GPT_CONFIG_124M)
+
+start_context = "Hello, I am a"
+encoded = tokenizer.encode(start_context)
+encoded_tensor = torch.tensor(encoded).unsqueeze(0)
+model.eval() #A
+out = generate_text_simple(
+    model=model,
+    idx=encoded_tensor,
+    max_new_tokens=6,
+    context_size=GPT_CONFIG_124M["context_length"]
+)
+decoded_text = tokenizer.decode(out.squeeze(0).tolist())
+print(decoded_text)
+
