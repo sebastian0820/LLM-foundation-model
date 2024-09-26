ecaac574a20757f090cfabef88464468d26fd0f9
sebastian0820 <sebastian.plata.dev@gmail.com>
2024-09-26 00:05:44 -0400

    updated

--- 5.py
+++ 5.py
@@ -47,34 +47,49 @@ inputs = torch.tensor([[16833, 3626, 6100], # ["every effort moves",
 targets = torch.tensor([[3626, 6100, 345 ], # [" effort moves you",
                         [588, 428, 11311]]) # " really like chocolate"]
 with torch.no_grad():logits = model(inputs)
-probas = torch.softmax(logits, dim=-1) # Probability of each token in vocabulary
+
+#show result of evaluation
+
+# probas = torch.softmax(logits, dim=-1) # Probability of each token in vocabulary
 # print(probas.shape)                                                                   #print tokens shape
-token_ids = torch.argmax(probas, dim=-1, keepdim=True)
+# token_ids = torch.argmax(probas, dim=-1, keepdim=True)
 # print("Token IDs:\n", token_ids)                                                    #print tokens shape
 # print()
+
+#manual calculation of loss
+
 # print(f"Targets batch 1: {token_ids_to_text(targets[0], tokenizer)}")               #print result for first token
 # print(f"Outputs batch 1: {token_ids_to_text(token_ids[0].flatten(), tokenizer)}")
 # print()
 # print(f"Targets batch 2: {token_ids_to_text(targets[1], tokenizer)}")               #print result for second token
 # print(f"Outputs batch 2: {token_ids_to_text(token_ids[1].flatten(), tokenizer)}")
 # print()
-text_idx = 0                                                                        #probability array for each token
-target_probas_1 = probas[text_idx, [0, 1, 2], targets[text_idx]]
+# text_idx = 0                                                                        #probability array for each token
+# target_probas_1 = probas[text_idx, [0, 1, 2], targets[text_idx]]
 # print("Text 1:", target_probas_1)
 # print()
-text_idx = 1
-target_probas_2 = probas[text_idx, [0, 1, 2], targets[text_idx]]
+# text_idx = 1
+# target_probas_2 = probas[text_idx, [0, 1, 2], targets[text_idx]]
 # print("Text 2:", target_probas_2)
 # print()
-log_probas = torch.log(torch.cat((target_probas_1, target_probas_2)))
-print(log_probas)
-print()
-avg_log_probas = torch.mean(log_probas)
-print(avg_log_probas)
-print()
-neg_avg_log_probas = avg_log_probas * -1
-print(neg_avg_log_probas)
-print()
-# loss = neg_avg_log_probas.backward()
-# print(loss)
+# log_probas = torch.log(torch.cat((target_probas_1, target_probas_2)))
+# print(log_probas)
 # print()
+# avg_log_probas = torch.mean(log_probas)
+# print(avg_log_probas)
+# print()
+# neg_avg_log_probas = avg_log_probas * -1
+# print(neg_avg_log_probas)
+# print()
+
+logits_flat = logits.flatten(0, 1)
+targets_flat = targets.flatten()
+print("Flattened logits:", logits_flat.shape)
+print("Flattened targets:", targets_flat.shape)
+
+print()
+loss = torch.nn.functional.cross_entropy(logits_flat, targets_flat)
+print(loss)
+
+perplexity = torch.exp(loss)
+print(perplexity)
